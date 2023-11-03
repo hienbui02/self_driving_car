13b41157b2c12edbb7110779960e91984590d952
nkcong206 <nkcong206@gmail.com>
2023-11-03 18:25:17 +0900

    planning

---
+++ ROS/gps/gps/gps.py
@@ -0,0 +1,54 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+import serial
+import threading
+
+gps_data = [0.0,0.0]
+gps_status = 1
+#upload location
+def read_gps_thread():
+    global gps_data, gps_status
+    with serial.Serial('/dev/ttyUSB1', 9600, timeout=10000) as ser:
+        while True:
+            data = ""
+            x = ser.readline()
+            line = x.decode('utf-8', errors='ignore')
+            if line.find("localtion") != -1:
+                line = line.replace("\t", "").replace("\n", "")
+                line = line.replace('"', '')
+                data = line.split(":")[1]
+        
+                gps_data[0] = data.split(",")[0]
+                gps_data[1] = data.split(",")[1]
+                
+                ser.close()
+            else:
+                gps_status = 0
+
+
+class gps_pubisher(Node):
+    def __init__(self, **kwargs):
+        super().__init__('gps node')
+        self.get_logger().info("gps Started")
+        self.gps_pub = self.create_publisher(Float32MultiArray, "/gps", 10) 
+        timer_period = 0.03
+        self.timer = self.create_timer(timer_period, self.gps_callback)
+        
+    def gps_callback(self):
+        global gps_data, gps_status
+        my_gps = Float32MultiArray()
+        my_gps.data = gps_data
+        my_gps.data.append(gps_status)
+        self.gps_pub.publish(my_gps)
+
+def main(args=None):
+    read_gps = threading.Thread(target=read_gps_thread, args=(1,))
+    read_gps.start()
+    rclpy.init(args=args)
+    gps_pub = gps_pubisher()
+    rclpy.spin(gps_pub)
+    gps_pub.destroy_node()
+    rclpy.shutdown()
+if __name__ == "__main__":
+    main()
---
+++ ROS/socketio/socketio/socketio.py
@@ -0,0 +1,90 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+import socketio
+from std_msgs.msg import Float32MultiArray
+
+gps_data = [0.0,0.0]
+gps_status = 0
+class SocketIOListener(Node):
+    def __init__(self):
+        super().__init__('socketio_listener')
+        self.SERVER_SOCKETIO = "http://192.168.200.184:5001"
+        self.ID = "robot1"
+        self.NAME = "123"
+        self.auto_publisher = self.create_publisher(Bool, '/automatic', 10)
+        self.cmd_vel_sub = self.create_subscription(Float32MultiArray, "/gps", self.gps_callback, 10)
+        self.sio = socketio.Client()
+
+        @self.sio.event
+        def connect():
+            self.get_logger().info('Socket.IO connected')
+
+        @self.sio.event
+        def disconnect():
+            self.get_logger().info('Socket.IO disconnected')
+            
+        
+        # Define event handlers
+        @self.sio.on('connect')
+        def on_connect():
+            print("Connected to server ...")
+            self.sio.emit("register_controller", {"robot_id" : self.ID, "robot_name" : self.NAME})
+
+        @self.sio.on('register_controller')
+        def on_message(data):
+            print("Message received:", data)
+            
+        @self.sio.on("robot_location") 
+        def thread_location():
+            global gps_data
+            while True:    
+                try:
+                    self.sio.emit("robot_location",{"robot_id" : self.ID, "location": gps_data})
+                    self.time.sleep(0.1)
+                    print("send to server")
+                except:
+                    pass
+           
+        @self.sio.on("automatic")
+        def on_run_automatic(data):
+            print(data['type'])
+            msg = Bool()
+            if data['type'] == 'Go':
+                msg.data = True
+            else:
+                msg.data = False
+            self.auto_publisher.publish(msg)
+            self.get_logger().info('Publishing: "%s"' % msg.data)
+                 
+        @self.sio.on('disconnect')
+        def on_disconnect():
+            print("Disconnected from server")
+    
+    def gps_callback(self, data_msg: Float32MultiArray):
+        global gps_data, gps_status
+        gps_data = data_msg.data[0:2]
+        gps_status = data_msg.data[2]
+        
+        
+    def start(self):
+        self.sio.connect(self.SERVER_SOCKETIO) # Replace with your Socket.IO server URL
+        rclpy.spin(self)
+
+    def stop(self):
+        self.sio.disconnect()
+
+def main(args=None):
+    rclpy.init(args=args)
+    socketio_listener = SocketIOListener()
+    try:
+        socketio_listener.start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        socketio_listener.stop()
+        socketio_listener.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
